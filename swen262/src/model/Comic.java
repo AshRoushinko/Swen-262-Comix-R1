8596824da6194167ff901cc6dd84342bae348320
AshRoushinko <ashroushinko@gmail.com>
2022-10-11 11:10:44 -0400

    update

---
+++ swen262/src/model/Comic.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.Flow.Publisher;
+
+public class Comic {
+
+    private String publisher, title, issue, publicationDate, creators, characters, description;
+    private int volume;
+    private double value;
+
+    public Comic(String Publisher, String Title, int Volume, String Issue, String PublicationDate){
+        this.publisher = Publisher;
+        this.title = Title;
+        this.volume = Volume;
+        this.issue = Issue;
+        this.publicationDate = PublicationDate;
+    }
+
+}
