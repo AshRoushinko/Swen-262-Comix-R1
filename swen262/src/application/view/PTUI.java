e837aed44b884916148b5fd2d7455419d9e5a198
jwk8288 <54957553+jwk8288@users.noreply.github.com>
2022-10-11 19:26:33 -0400

    test

--- swen262/src/application/view/PTUI.java
+++ swen262/src/application/view/PTUI.java
@@ -2,7 +2,26 @@ package view;
 
 public class PTUI {
 
+
+    //Constructor
     public PTUI() {
+
+    }
+
+    /**
+     * This method handles any input from the user and sends the information to the App class. The result of the operation will be printed on the
+     * screen for the user to see.
+     */
+    public void handle(){
+
+    }
+
+    /**
+     * This method handles the search operation.
+     * Param - content: includes a string representing a name or partial name of the comic(s) to search for.
+     * Return - The Book(s) that were found in the search.
+     */
+    private void search(String content){
 
     }
 
