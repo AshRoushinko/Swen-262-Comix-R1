eee10c64fdc82f116f69af49bd48ba12e2d98835
jwk8288 <54957553+jwk8288@users.noreply.github.com>
2022-10-11 19:07:23 -0400

    Created TODOs; created methods with no implementation

--- swen262/src/application/controller/App.java
+++ swen262/src/application/controller/App.java
@@ -1,4 +1,24 @@
 package controller;
 
+import model.Database;
+import model.User;
+import view.PTUI;
+
 public class App {
+
+    private Database db;
+    private User user;
+    private PTUI view;
+
+    public App(){
+    }
+
+    public void init(){
+
+    }
+
+    public void process(){
+        //TODO this method will process the commands sent from the PTUI. Allows for all other methods to be private
+    }
+
 }
--- swen262/src/application/view/PTUI.java
+++ swen262/src/application/view/PTUI.java
@@ -1,4 +1,17 @@
 package view;
 
 public class PTUI {
+
+    public PTUI() {
+
+    }
+
+    public void init(){
+        System.out.println("WELCOME TO COMIX!!!\n" +
+                "A comic book database.");
+
+        //TODO print a list of all the commands the user can use and implement. Command info will be sent back to the App class
+
+    }
+
 }
